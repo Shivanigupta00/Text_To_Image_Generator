7500aaaaeb22851b93f9738be1f002611876b720
Shivanigupta00 <shivani04112003.sg@gmail.com>
2024-11-29 19:56:29 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+
+import 'package:text_to_image_app/AI%20Image%20Generator/image_generator.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AiTextToImageGenerator(),
+    );
+  }
+}
+      
+      
